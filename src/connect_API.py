922f9adedd41709e614ea5736193512d78cddbc1
Nosheen005 <nosheen.yasheen05@gmail.com>
2025-02-17 15:03:28 +0100

    producer and changes in connect_api

--- src/connect_API.py
+++ src/connect_API.py
@@ -1,33 +1,30 @@
-from requests import Session,TooManyRedirects,Timeout
+from requests import Session
 import json
 from pprint import pprint
 from constants import COINMARKET_API
 
 def get_latest_coin_data(symbol="ADA"):
-    API_KEY = COINMARKET_API
-    api_url = "https://pro-api.coinmarketcap.com/v1/cryptocurrency/quotes/latest"
-
-    headers = {
-        "Accepts": "application/json",
-        "X-CMC_PRO_API_KEY": API_KEY,
-    }
+    API_URL = "https://pro-api.coinmarketcap.com/v1/cryptocurrency/quotes/latest"
+    
+    symbol = "ADA"
+    
 
     parameters = {
         "symbol": symbol,
         "convert": "USD",
     }
 
+    headers = {
+        "Accepts": "application/json",
+        "X-CMC_PRO_API_KEY": COINMARKET_API,
+    }
+
     session = Session()
     session.headers.update(headers)
-    try:
-        response = session.get(api_url, params=parameters)
-        return json.loads(response.text).get("data").get(symbol)
-    except (ConnectionError, Timeout, TooManyRedirects) as e:
+    
+    response = session.get(API_URL, params=parameters)
+    print(response.text)
+    #return json.loads(response.text)["data"][symbol]
 
-        print(e)
-
-
-if __name__ == "__main__":
-    result = get_latest_coin_data ("SOL")
-    pprint(result)
+get_latest_coin_data()
 
